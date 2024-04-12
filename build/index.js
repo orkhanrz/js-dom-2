a480c8f5a1e4ef3c567b796084ce1e9da26c39fe
Orkhan Rzali <orkhan.rz8@gmail.com>
2024-04-12 15:53:36 +0400

    task 10 done

--- build/index.js
+++ build/index.js
@@ -96,13 +96,16 @@ taskItemButtons.forEach((btn) => {
     });
 });
 taskInputBtn.addEventListener("click", () => {
+    removeActiveClasses(taskItemParagraphs);
     const tasksToDoEl = document.querySelector(".taskItemContainer");
     const newElement = document.createElement("p");
     newElement.textContent = taskInput.value;
     tasksToDoEl.insertAdjacentElement("beforeend", newElement);
     taskInput.value = "";
     taskItemParagraphs = document.querySelectorAll(".taskItem .taskItemContainer p");
-    newElement.addEventListener("click", () => addActiveClass(newElement));
+    newElement.addEventListener("click", () => {
+        addActiveClass(newElement);
+    });
 });
 // Task 4
 const imageItems = document.querySelectorAll(".imageItem");
@@ -554,3 +557,45 @@ function deleteUser(id) {
 fetchUsers();
 renderUsers();
 addUserBtn.addEventListener('click', () => openModal('create'));
+// Task 10
+const assessmentRowElements = document.querySelectorAll('.assessment table tbody tr p');
+const assessmentModal = document.querySelector('.assessmentModal');
+const assessmentMarks = document.querySelectorAll('.assessmentModalItems p');
+const rowHeadElements = document.querySelectorAll('.rowHeadEl');
+const closeAssessmentModalBtn = document.querySelector('.closeAssessment');
+let activeAssessmentElement;
+function toggleAssessmentModal() {
+    assessmentModal.classList.toggle('active');
+}
+;
+function swapElementValue(targetEl, swapEl) {
+    targetEl.innerHTML = swapEl.innerHTML;
+    targetEl.classList.remove(targetEl.classList.value);
+    targetEl.classList.add(swapEl.classList[0]);
+}
+;
+closeAssessmentModalBtn.addEventListener('click', toggleAssessmentModal);
+assessmentRowElements.forEach(el => {
+    el.addEventListener('click', function () {
+        activeAssessmentElement = this;
+        toggleAssessmentModal();
+    });
+});
+assessmentMarks.forEach(mark => {
+    mark.addEventListener('click', function () {
+        swapElementValue(activeAssessmentElement, this);
+        toggleAssessmentModal();
+    });
+});
+rowHeadElements.forEach(el => {
+    el.addEventListener('click', function () {
+        const columns = Array.from(this.parentElement.parentElement.children);
+        const index = this.parentElement && columns.indexOf(this.parentElement) + 3;
+        const tableColElements = document.querySelectorAll(`.assessment table tbody tr td:nth-child(${index}) p`);
+        tableColElements.forEach((colEl) => {
+            if (colEl.classList.contains('black')) {
+                swapElementValue(colEl, this);
+            }
+        });
+    });
+});
