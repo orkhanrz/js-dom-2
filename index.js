0a67f98c136aa132cb5b9af6849a113ce8ac12b2
Orkhan Rzali <orkhan.rz8@gmail.com>
2024-04-18 19:38:21 +0400

    quiz finished

--- index.js
+++ index.js
@@ -284,10 +284,12 @@ const soundPercentageEl = document.querySelector(".soundControlPercentage");
 const soundProgressWidth = 500;
 const soundProgressChangePercentage = 5;
 let soundPercentage = 50;
+
 function controlSound(soundPercentage) {
 	soundProgress.style.width = soundPercentage + "%";
 	soundPercentageEl.innerText = soundPercentage + "%";
 }
+
 soundActions.forEach((btn) => {
 	btn.addEventListener("click", function () {
 		if (
@@ -305,6 +307,7 @@ soundActions.forEach((btn) => {
 		controlSound(soundPercentage);
 	});
 });
+
 soundProgressBar.addEventListener("click", function (e) {
 	const percentage = Math.floor(
 		(100 * Math.floor(e.clientX - this.getBoundingClientRect().left)) /
@@ -313,6 +316,7 @@ soundProgressBar.addEventListener("click", function (e) {
 	soundPercentage = percentage;
 	controlSound(soundPercentage);
 });
+
 // Task 7
 const activeImg = document.querySelector(".activeImage img");
 const inactiveImagesDiv = document.querySelector(".inActiveImages");
@@ -437,17 +441,22 @@ createItems(50);
 createPages();
 disableBtns();
 fetchData(activePage);
+
+//Task 9
 const usersTableBody = document.querySelector(".panelContent table tbody");
 const addUserBtn = document.querySelector("#addUser");
 let deleteUserBtns = document.querySelectorAll(".deleteUserBtn");
 let editUserBtns = document.querySelectorAll(".editUserBtns");
 let users = [];
+
 function fetchUsers() {
 	users = JSON.parse(localStorage.getItem("users")) || [];
 }
+
 function updateUsers(users) {
 	localStorage.setItem("users", JSON.stringify(users));
 }
+
 function renderUsers() {
 	usersTableBody.innerHTML = "";
 	let usersHTML = ``;
@@ -466,14 +475,17 @@ function renderUsers() {
 		});
 	}
 	usersTableBody.innerHTML = usersHTML;
+
 	deleteUserBtns = document.querySelectorAll(".deleteUserBtn");
 	editUserBtns = document.querySelectorAll(".editUserBtn");
+
 	deleteUserBtns.forEach((btn) => {
 		btn.addEventListener("click", function () {
 			const id = this.parentElement.id;
 			deleteUser(+id);
 		});
 	});
+
 	editUserBtns.forEach((btn) => {
 		btn.addEventListener("click", function () {
 			const id = +this.parentElement.id;
@@ -606,12 +618,15 @@ function deleteUser(id) {
 	updateUsers(users);
 	renderUsers();
 }
+
 fetchUsers();
 renderUsers();
 addUserBtn.addEventListener("click", () => openModal("create"));
 
 // Task 10
-const assessmentRowElements = document.querySelectorAll(".assessment table tbody tr p");
+const assessmentRowElements = document.querySelectorAll(
+	".assessment table tbody tr p"
+);
 const assessmentModal = document.querySelector(".assessmentModal");
 const assessmentMarks = document.querySelectorAll(".assessmentModalItems p");
 const rowHeadElements = document.querySelectorAll(".rowHeadEl");
@@ -649,22 +664,22 @@ rowHeadElements.forEach((el) => {
 		tableColElements.forEach((colEl) => {
 			if (colEl.classList.contains("black")) {
 				swapElementValue(colEl, this);
-			};
+			}
 		});
 	});
 });
 
 // Task 11
-const ilhamInputWrapper = document.querySelector('.ilhamInputWrapper');
+const ilhamInputWrapper = document.querySelector(".ilhamInputWrapper");
 const ilhamInput = document.querySelector(".ilham input");
 const ilhamBtn = document.querySelector(".ilham button");
 const targetHeadCol = document.querySelector(".targetHeadCol");
 const targetBodyCols = document.querySelectorAll(".targetBodyCol");
 let mark = 0;
-let rowMarks = [0,0,0,0,0];
+let rowMarks = [0, 0, 0, 0, 0];
 
-function closeAssessmentTool(){
-	document.querySelectorAll('.tool').forEach(tool => tool.remove());
+function closeAssessmentTool() {
+	document.querySelectorAll(".tool").forEach((tool) => tool.remove());
 }
 
 function openAssessmentTool(cell, row) {
@@ -707,31 +722,430 @@ function createElements(length) {
 			targetBodyCell.before(td);
 
 			button.addEventListener("click", () => {
-				openAssessmentTool(td, i + 1)
+				openAssessmentTool(td, i + 1);
 			});
 		});
 	}
 }
 
-function markPerson(row, cell, btn){
+function markPerson(row, cell, btn) {
 	cell.children[0].textContent = btn.textContent;
 	cell.children[0].classList.remove(cell.children[0].classList.value);
 	cell.children[0].classList.add(btn.classList.value);
 
-	if (btn.className.includes('green')) {
-		rowMarks[row]+= mark;
-	} else if (btn.className.includes('yellow')) {
-		rowMarks[row]+= mark / 2;
+	if (btn.className.includes("green")) {
+		rowMarks[row] += mark;
+	} else if (btn.className.includes("yellow")) {
+		rowMarks[row] += mark / 2;
 	} else {
-		rowMarks[row]+= 0;
+		rowMarks[row] += 0;
 	}
 
 	targetBodyCols[row - 1].textContent = rowMarks[row].toFixed(2);
 	cell.children[0].disabled = true;
 	closeAssessmentTool();
-};
+}
 
 ilhamBtn.addEventListener("click", () => {
 	createElements(+ilhamInput.value);
 	ilhamInputWrapper.remove();
 });
+
+// Task 12
+var quiz = {
+	az: {
+		easy: [
+			{
+				question: "Azərbaycan dilində neçə sait var?",
+				A: "A)8",
+				B: "B)9",
+				C: "C)7",
+				D: "D)11",
+				correct: "B",
+			},
+			{
+				question: "Azərbaycan dilində neçə samit səs var?",
+				A: "A)34",
+				B: "B)23",
+				C: "C)25",
+				D: "D)32",
+				correct: "C",
+			},
+			{
+				question: "Hansı kar samitin cingiltili qarşılığı yoxdur?",
+				A: "A)H",
+				B: "B)F",
+				C: "C)G",
+				D: "D)C",
+				correct: "A",
+			},
+			{
+				question: "Əlifbada neçə hərf var?",
+				A: "A)30",
+				B: "B)23",
+				C: "C)32",
+				D: "D)34",
+				correct: "C",
+			},
+			{
+				question: "Bitişdirici samitləri göstərin:",
+				A: "A)d,t,l",
+				B: "B)n,m,s",
+				C: "C)m,l,k",
+				D: "D)n,y,s",
+				correct: "D",
+			},
+		],
+		medium: [
+			{
+				question: "Qrammatik cəhətdən feil bildirir?",
+				A: "A)Hərəkət",
+				B: "B)Xususiyyət",
+				C: "C)Keyfiyyət",
+				D: "D)Əlamət",
+				correct: "A",
+			},
+			{
+				question: "Feilin neçə sadə şəkli var?",
+				A: "A)4",
+				B: "B)5",
+				C: "C)6",
+				D: "D)8",
+				correct: "C",
+			},
+			{
+				question: "Feil haqqında hansı fikir səhvdir?",
+				A: "A)feilin dərəcələri var",
+				B: "B)feil hərəkət bildirir",
+				C: "C)feil təsirli və təsirsiz olur",
+				D: "D)feilin 3 zamanı var",
+				correct: "A",
+			},
+			{
+				question: "Feilin quruluşca neçə novu var?",
+				A: "A)5",
+				B: "B)6",
+				C: "C)4",
+				D: "D)3",
+				correct: "D",
+			},
+			{
+				question: "Feil cümlədə əsasən hansı cümlə üzvü olur?",
+				A: "A)təyin",
+				B: "B)mübtəda",
+				C: "C)xəbər",
+				D: "D)zərflik",
+				correct: "C",
+			},
+		],
+		hard: [
+			{
+				question:
+					"Hansı əlaqə növü bütün tabeli mürəkkəb cümlələrdə olur?",
+				A: "A)Qarşılaşdırma",
+				B: "B)Zaman",
+				C: "C)Səbəb-nəticə",
+				D: "D)Heç biri",
+				correct: "B",
+			},
+			{
+				question: "Mürəkkəb cümlənin hansı növləri var?",
+				A: "A)tabeli, tabesiz",
+				B: "B)düzəltmə",
+				C: "C)əmr",
+				D: "D)sadə",
+				correct: "A",
+			},
+			{
+				question: "Mürəkkəb cümlənin neçə novu var?",
+				A: "A)6",
+				B: "B)2",
+				C: "C)5",
+				D: "D)3",
+				correct: "D",
+			},
+			{
+				question:
+					"Hansı bağlayıcı merəkkəb cümlənin tərəflərini bağlamır?",
+				A: "A)ancaq",
+				B: "B)ilə",
+				C: "C)belə ki",
+				D: "D)yəni",
+				correct: "B",
+			},
+			{
+				question: "Mürəkkəb cümlələr neçə növə ayrılır?",
+				A: "A)2",
+				B: "B)4",
+				C: "C)5",
+				D: "D)3",
+				correct: "A",
+			},
+		],
+	},
+	math: {
+		easy: [
+			{
+				question: "5 %-i 2 olan ədədi tap?",
+				A: "A)43",
+				B: "B)40",
+				C: "C)49",
+				D: "D)35",
+				correct: "A",
+			},
+			{
+				question: "(x+3)+(x+1)=12 Tənliyi həll edin?",
+				A: "A)3",
+				B: "B)5",
+				C: "C)4",
+				D: "D)35",
+				correct: "C",
+			},
+			{
+				question: "Sürəti 7 olan neçə düzgün olmayan kəsr var?",
+				A: "A)7",
+				B: "B)32",
+				C: "C)4",
+				D: "D)6",
+				correct: "A",
+			},
+			{
+				question: "4%-i 5,6 olan ədədi tapın:",
+				A: "A)48",
+				B: "B)400",
+				C: "C)49",
+				D: "D)140",
+				correct: "D",
+			},
+			{
+				question:
+					"0,1(3) sonsuz dövrü onluq kəsrini adi kəsrə çevirin.",
+				A: "A)2/15",
+				B: "B)26/15",
+				C: "C)12/15",
+				D: "D)7/15",
+				correct: "A",
+			},
+		],
+		medium: [
+			{
+				question:
+					"İki ədədin cəmi 5, fərqi 3-dür.Bu ədədlərin hasilini tapın?",
+				A: "A)4",
+				B: "B)7",
+				C: "C)9",
+				D: "D)5",
+				correct: "A",
+			},
+			{
+				question: "X≠0 olarsa, 2x(x+y)=5y,x(x-y)=y olduqda x-i tapın?",
+				A: "A)3/4",
+				B: "B)5",
+				C: "C)4/9",
+				D: "D)38",
+				correct: "A",
+			},
+			{
+				question:
+					"x+y=5,x-y=1 olduğuna görə, x2-y2+3x+3y ifadəsinin qiymətini tapın?",
+				A: "A)10",
+				B: "B)20",
+				C: "C)30",
+				D: "D)40",
+				correct: "B",
+			},
+			{
+				question:
+					"2x+y=5,x-3y=-5 tənliklər sistemindən x+y cəmini tapın?",
+				A: "A)4",
+				B: "B)7",
+				C: "C)3",
+				D: "D)5",
+				correct: "C",
+			},
+			{
+				question: "x+3=2(y-4),y-2=4x tənliklər sistemindən x-i tapın:",
+				A: "A)1/15",
+				B: "B)2/15",
+				C: "C)3/15",
+				D: "D)4/15",
+				correct: "B",
+			},
+		],
+		hard: [
+			{
+				question: "Hesablayın: (9-2√10)(9+2√10)?",
+				A: "A)36",
+				B: "B)12",
+				C: "C)12√10",
+				D: "D)41",
+				correct: "D",
+			},
+			{
+				question: "Hesablayın: (3√5+√15)2-10√27?",
+				A: "A)80",
+				B: "B)70",
+				C: "C)60",
+				D: "D)50",
+				correct: "C",
+			},
+			{
+				question: "√(5+2√6)=?",
+				A: "A)√2+√6",
+				B: "B)√3+√2",
+				C: "C)√5+√2",
+				D: "D)√3+√5",
+				correct: "B",
+			},
+			{
+				question: "A=√6+1 və b=√6 -1 olduğuna görə, a:b+b:a=?",
+				A: "A)2,8",
+				B: "B)3,8",
+				C: "C)3",
+				D: "D)2",
+				correct: "A",
+			},
+			{
+				question: "(x-3)2+(y+7)2=0 isə, xy=?",
+				A: "A)23",
+				B: "B)-32",
+				C: "C)21",
+				D: "D)-21",
+				correct: "D",
+			},
+		],
+	},
+};
+
+const step1 = document.querySelector(".step-1");
+const step2 = document.querySelector(".step-2");
+const step3 = document.querySelector(".step-3");
+const step4 = document.querySelector(".step-4");
+const quizPages = document.querySelector(".controls .pages");
+const question = document.querySelector(".question");
+const options = document.querySelector(".options");
+const optionsBtns = document.querySelectorAll('.options button');
+const previous = document.querySelector(".leftBtn");
+const next = document.querySelector(".rightBtn");
+
+let subject;
+let level;
+let questions = [];
+let questionIndex = 0;
+let rightAnswers = 0;
+let wrongAnswers = 0;
+let notAnswered = 0;
+
+function showQuestion(questionIndex) {
+	let currentQuestion = questions[questionIndex];
+
+	quizPages.childNodes.forEach((page) => page.classList.remove("active"));
+	quizPages.childNodes[questionIndex].classList.add("active");
+	optionsBtns.forEach(btn => {
+		btn.classList.remove('wrong');
+		btn.classList.remove('selected');
+	});
+
+	question.innerText = currentQuestion.question;
+	A.textContent = currentQuestion.A;
+	B.textContent = currentQuestion.B;
+	C.textContent = currentQuestion.C;
+	D.textContent = currentQuestion.D;
+}
+
+function createQuizPages(questions) {
+	const pages = questions.length;
+
+	for (let i = 1; i <= pages; i++) {
+		const page = document.createElement("button");
+		page.innerText = i;
+
+		quizPages.appendChild(page);
+
+		page.addEventListener("click", function () {
+			questionIndex = i - 1;
+			showQuestion(questionIndex);
+		});
+	}
+}
+
+function endGame(){
+	notAnswered = questions.length - (wrongAnswers + rightAnswers);
+
+	correctCount.textContent = rightAnswers;
+	wrongCount.textContent = wrongAnswers;
+	notAnsweredCount.textContent = notAnswered;
+	totalCount.textContent = questions.length;
+
+	step3.style.display = 'none';
+	step4.style.display = 'block';
+};
+
+function restartGame(){
+	subject = null;
+	level = null;
+	questions = [];
+	questionIndex = 0;
+	rightAnswers = 0;
+	wrongAnswers = 0;
+	notAnswered = 0;
+	quizPages.innerHTML = '';
+
+	step4.style.display = 'none';
+	step1.style.display = 'block';
+};
+
+step1.childNodes.forEach((btn) => {
+	btn.addEventListener("click", function () {
+		subject = this.id;
+		step1.style.display = "none";
+		step2.style.display = "block";
+	});
+});
+
+step2.childNodes.forEach((btn) => {
+	btn.addEventListener("click", function () {
+		level = this.id;
+		questions = quiz[subject][level];
+		step2.style.display = "none";
+		step3.style.display = "block";
+
+		createQuizPages(questions);
+		showQuestion(questionIndex);
+	});
+});
+
+next.addEventListener("click", () => {
+	if (questionIndex < questions.length - 1) {
+		questionIndex++;
+
+		showQuestion(questionIndex);
+	} else {
+		endGame();
+	}
+});
+
+previous.addEventListener("click", () => {
+	if (questionIndex > 0) {
+		questionIndex--;
+
+		showQuestion(questionIndex);
+	}
+});
+
+optionsBtns.forEach((option) => {
+	option.addEventListener("click", function () {
+		const correctAnswer = questions[questionIndex]["correct"];
+
+		if (this.id === correctAnswer) {
+			rightAnswers++;
+			this.classList.add("selected");
+		} else {
+			wrongAnswers++;
+			this.classList.add("wrong");
+			document.querySelector(".options #" + correctAnswer).classList.add("selected");
+		}
+	});
+});
+
+restart.addEventListener('click', restartGame);
